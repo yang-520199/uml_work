d65847cde91fa4d8173044d5d2e1bc9f9851bdb2
yang-520199 <951333953@qq.com>
2019-12-07 01:40:28 +0800

    增加图片上传并且存储图片

---
+++ src/manager/deo/alter_food_Servlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "alter_food_Servlet")
+public class alter_food_Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
---
+++ src/manager/deo/delete_food_Servlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "delete_food_Servlet")
+public class delete_food_Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
